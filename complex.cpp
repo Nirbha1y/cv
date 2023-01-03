9bc4b7119d02f13c496aacf0cae1b71804559ae0
Nirbhay Sunuwar <nrbhaysunuwar@gmail.com>
2023-01-03 07:15:46 +0545

    First file made

---
+++ complex.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+  class Complex{
+      
+      int img,real;
+      public:
+   
+       void set()
+       {
+        cout<<"ENter real"<<endl;
+        cin>>real;
+        cout<<"Enter img"<<endl;
+        cin>>img;
+       }
+
+       void addComplex(Complex C1,Complex C2)
+       {
+
+        real=c1.real+c2.real;
+        img=c1.img+c2.img:
+        
+       }
+    
+  }
+
+
+
+  int main ()
+  {
+
+
+
+    return 0;
+  }
